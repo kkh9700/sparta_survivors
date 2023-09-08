9c439796d4a0d4495362b1a0d10d8e4c25aec881
김경환 <kh.kim9700@gmail.com>
2023-09-08 19:44:16 +0900

    불릿타임 게이지로 변경, 대쉬 쿨타임으로 변경

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Image HP;
+    [SerializeField] private Image HPImg;
+    [SerializeField] private Image BulletTimeImg;
+    [SerializeField] private Image DashImg;
     [SerializeField] private GameObject BulletTimeEffect;
     private Camera _camera;
     private Rigidbody2D _rigidbody;
@@ -17,18 +16,47 @@ public class PlayerController : MonoBehaviour
     private float speed;
     private int maxHP = 10;
     private bool isBulletTime = false;
+    private float BTGauge = 5;
+    private float BTScale = 3;
+    private float DashCoolTime = 2;
+    private float DamagedDelay = 0;
 
     private void Awake()
     {
         _camera = Camera.main;
         _rigidbody = GetComponent<Rigidbody2D>();
-        _renderer = GetComponent<SpriteRenderer>();
+        _renderer = GetComponentInChildren<SpriteRenderer>();
         player = new Player(maxHP, 1, 5, 1);
         speed = player.Speed;
     }
 
     private void Update()
     {
+        if (isBulletTime)
+        {
+            BTGauge -= Time.deltaTime * BTScale;
+            if (BTGauge < 0)
+                OffBulletTime();
+        }
+        else
+        {
+            BTGauge += Time.deltaTime * .5f;
+            if (BTGauge > 5)
+                BTGauge = 5;
+        }
+
+        DashCoolTime += Time.deltaTime / Time.timeScale;
+
+        if (DashCoolTime > 2)
+            DashCoolTime = 2;
+
+        DamagedDelay -= Time.deltaTime;
+
+        if (DamagedDelay < 0)
+            DamagedDelay = 0;
+
+        BulletTimeImg.fillAmount = BTGauge / 5f;
+        DashImg.fillAmount = DashCoolTime / 2f;
     }
 
     private void FixedUpdate()
@@ -54,7 +82,11 @@ public class PlayerController : MonoBehaviour
 
     public void OnDash()
     {
-        speed = 50;
+        if (DashCoolTime < 2)
+            return;
+
+        DashCoolTime = 0;
+        speed += 50;
         Invoke("OffDash", .05f);
     }
 
@@ -62,23 +94,26 @@ public class PlayerController : MonoBehaviour
     {
         if (!isBulletTime)
         {
-            Time.timeScale = .5f;
-            speed *= 2;
-            BulletTimeEffect.SetActive(true);
+            Time.timeScale = 1 / BTScale;
+            speed *= BTScale;
             isBulletTime = true;
-            Invoke("OffBulletTime", 5f);
+            BulletTimeEffect.SetActive(true);
+        }
+        else
+        {
+            OffBulletTime();
         }
     }
 
     private void OffDash()
     {
-        speed = player.Speed;
+        speed -= 50;
     }
 
     private void OffBulletTime()
     {
         Time.timeScale = 1;
-        speed /= 2;
+        speed /= BTScale;
         isBulletTime = false;
         BulletTimeEffect.SetActive(false);
     }
@@ -96,13 +131,13 @@ public class PlayerController : MonoBehaviour
             _renderer.flipX = direction.x < 0;
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
-        Debug.Log("Collision");
-        if (collision.gameObject.tag == "Monster")
+        if (DamagedDelay <= 0 && collision.gameObject.tag == "Monster")
         {
+            DamagedDelay = .5f;
             player.Damaged(collision.transform.GetComponent<MonsterController>().GetAttack());
-            HP.fillAmount = player.HP / (float)maxHP;
+            HPImg.fillAmount = player.HP / (float)maxHP;
         }
     }
 }
