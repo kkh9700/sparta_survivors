ea74ca2a1a018cd7ec74a17b65691a4fc1023d7a
김경환 <kh.kim9700@gmail.com>
2023-09-08 15:38:18 +0900

    MonsterController

---
+++ Assets/Scripts/Controller/MonsterController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _renderer;
+    private Vector2 _movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        SetVector2();
+        ApplyMovment(_movementDirection);
+        Rotate(_movementDirection);
+    }
+
+    private void SetVector2()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        float x = player.transform.position.x - transform.position.x;
+        float y = player.transform.position.y - transform.position.y;
+        _movementDirection = new Vector2(x, y).normalized;
+    }
+
+    private void ApplyMovment(Vector2 direction)
+    {
+        direction = direction * 3;
+
+        _rigidbody.velocity = direction;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        if (direction.x != 0)
+            _renderer.flipX = direction.x < 0;
+    }
+}
